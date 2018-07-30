17a6f884eef4a77accf25540d9ecb49a445ea150
Jeroen van Ginneken <jeroenvanginneken@MacBook-Pro-van-Jeroen-6845.local>
2018-07-30 09:12:50 +0200

    Testing

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Wij zijn allround in web!',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Innomedio learning React</h1>
+    <p>Innomedio is een full-service webbureau. 
+      Wij bieden kmo's duurzame weboplossingen aan met meetbaar 
+      rendement.
+
+    </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
